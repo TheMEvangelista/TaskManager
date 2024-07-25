b3ca2444bb932a55a3c1ded47c52bc4ec1a6250d
jbunlay <jbunlay@gmail.com>
2024-07-25 14:44:36 +0000

    Chore: some changes

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className='button rounded-20 bg-blue-500'>
+      <button>Click Me</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <main>
+        <section>
+            <h1>Task Manager</h1>
+        </section>
+    </main>
+  )
+}
+
+export default Container
