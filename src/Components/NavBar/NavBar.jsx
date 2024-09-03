5a723e8f54db7e802a2f0512f4dc8a16b24becd2
jbunlay <jbunlay@gmail.com>
2024-09-03 20:49:33 +0000

    Restarting project

--- src/Components/NavBar/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const NavBar = () => {
-  return (
-    <main className='bg-background flex items-center justify-center px-6 py-2'>
-        <h2 className='text-xl font-medium text-white py-2'>TODO TASK</h2>
-    </main>
-  )
-}
