b29b1c2e437143a1e0a3d782371cf94deba73700
Matheus <vincejulls2517@gmail.com>
2024-09-12 01:08:23 -0300

    Chore: alteração dos conteúdos da NavBar

--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -20,18 +20,25 @@ const NavBar = ({ userInfo }) => {
   };
 
   return (
-    <section className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+    <section
+      className={`bg-white flex items-center px-6 py-2 drop-shadow ${
+        userInfo && Object.keys(userInfo).length > 0
+          ? "justify-between"
+          : "justify-center"
+      }`}>
       <h2 className="font-Matemasie text-xl font-medium text-black py-2">
         ...TODO TASK...
       </h2>
-      <SearchBar
-        value={searchQuery}
-        onChange={({ target }) => {
-          setSearchQuery(target.value);
-        }}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch}
-      />
+      {userInfo && Object.keys(userInfo).length > 0 && (
+        <SearchBar
+          value={searchQuery}
+          onChange={({ target }) => {
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+      )}
       {userInfo && Object.keys(userInfo).length > 0 && (
         <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
       )}
